2c7c5538f077506124aefaad6cef0f7d8ced748c
Peter Elliott <peter@layer.com>
2018-03-13 22:04:49 -0600

    AND-1268 Adding proper multidex support Only enabling for debug builds since it's only needed for tests

--- ui/src/androidTest/java/com/layer/xdk/ui/TestButlerRunner.java
+++ ui/src/androidTest/java/com/layer/xdk/ui/TestButlerRunner.java
@@ -1,12 +1,19 @@
 package com.layer.xdk.ui;
 
 import android.os.Bundle;
+import android.support.multidex.MultiDex;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnitRunner;
 
 import com.linkedin.android.testbutler.TestButler;
 
 public class TestButlerRunner extends AndroidJUnitRunner {
+
+    @Override
+    public void onCreate(Bundle arguments) {
+        MultiDex.install(getTargetContext());
+        super.onCreate(arguments);
+    }
 
     @Override
     public void onStart() {
